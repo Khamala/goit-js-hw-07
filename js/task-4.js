dc3470f8816e71145b7d0a6f62e3c3cc30a2bd54
Khamala <elektrikamegawolt@gmail.com>
2024-03-08 13:30:23 +0100

    update task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', submitForm);
+
+function submitForm(event) {
+    event.preventDefault();
+
+    const { email, password } = event.currentTarget.elements
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+    if (!emailValue || !passwordValue) {
+        return alert('All form fields must be filled in');
+    };
+
+    console.log({email: emailValue, password: passwordValue});
+
+    event.currentTarget.reset();
+};

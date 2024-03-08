ddf7bfc7cdacecb3e6d403a0459e853512cbe430
Khamala <elektrikamegawolt@gmail.com>
2024-03-08 13:52:48 +0100

    update task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,18 @@
+const btn = document.querySelector('.change-color');
+const text = document.querySelector('.color');
+
+btn.addEventListener('click', changeColor);
+
+function changeColor() {
+  // text.textContent = getRandomHexColor()
+
+  document.body.style.backgroundColor = text.textContent = getRandomHexColor();
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
+
+
